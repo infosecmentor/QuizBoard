a4f05fc73d5df360c0f6d1f30b1238372c292fac
infosecmentor <muhambiphares@gmail.com>
2022-05-14 15:18:39 +0300

    dealt with incomplete input

---
+++ app.js
@@ -0,0 +1,18 @@
+function result () {
+  var score=0;
+  if(document.getElementById('correct1').checked)
+  {
+      score++;
+  }
+  if(document.getElementById('correct2').checked)
+  {
+      score++;
+  }
+  if(document.getElementById('correct3').checked)
+  {
+      score++;
+  }
+  document.write("Your Score Is : "+score);
+  
+  
+}
